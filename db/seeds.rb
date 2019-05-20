73bed90a30809f00eef7ae55158d3ae0c85bfc0f
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-20 18:53:26 -0400

    fixed the db seed, missing comas

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,25 @@ Item.destroy_all if Rails.env.development?
 
 speaker = Item.create!(
   location: "Montreal - Laval",
-  name: "Great Powerfull Speaker"
-  description: "Set of 2 speakers Yamaha, great for garden party, super cool sound."
-  price: 10
-  condition: "1 year old. Great condition. Small hole on the top from cigarette butt."
-  deposit: 100
-  image_url: "https://www.listeningpost.co.nz/assets/Yamaaha-NS-5000-Speakers-Pair_183108_5.jpg"
-  delivery: "pick up at my place"
+  name: "Great Powerfull Speaker",
+  description: "Set of 2 speakers Yamaha great for garden party super cool sound.",
+  price: 10,
+  condition: "1 year old. Great condition. Small hole on the top from cigarette butt.",
+  deposit: 100,
+  image_url: "https://www.listeningpost.co.nz/assets/Yamaaha-NS-5000-Speakers-Pair_183108_5.jpg",
+  delivery: "pick up at my place",
   policies: "if you break it or there is another hole from cigarette butt, you will buy me a new one"
 )
 
 dj_set = Item.create!(
   location: "Toronto",
-  name: "Hercules DJ Control Inpulse 300"
-  description: "DJ set for a weekend discotheque"
-  price: 80
-  condition: "You get a perfect table to play your cool shit"
-  deposit: 500
-  image_url: "https://images-na.ssl-images-amazon.com/images/I/514J8t-BCSL._SL1032_.jpg"
-  delivery: "pick up at Toronto, Maple Leaves stadium"
+  name: "Hercules DJ Control Inpulse 300",
+  description: "DJ set for a weekend discotheque",
+  price: 80,
+  condition: "You get a perfect table to play your cool shit",
+  deposit: 500,
+  image_url: "https://images-na.ssl-images-amazon.com/images/I/514J8t-BCSL._SL1032_.jpg",
+  delivery: "pick up at Toronto, Maple Leaves stadium",
   policies: "dont break it"
   )
 
