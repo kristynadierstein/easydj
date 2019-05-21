38792c79a4191a52a6a1a79d42a4c99e6ea6f679
Kristyna Dierstein <kristynahorankova@gmail.com>
2019-05-21 11:57:46 -0400

    change in schema because of migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_141743) do
+ActiveRecord::Schema.define(version: 2019_05_21_153910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_05_21_141743) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -54,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_141743) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.string "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
