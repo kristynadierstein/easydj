ec2b6a356f0fec251d14d625bcd7ee32277558cf
Romain Penaruiz <rom.penaruiz@gmail.com>
2019-05-20 21:07:51 -0400

    created the method index, show, create, new, destroy. Created the index show page displaying two speakers, with a create new button and delete button. The create new direct you to a new page and you can add a new item but it does not save. I need to be loged in in order for it to work.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,24 +3,43 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-    raise
-  end
-
-  def create
-  end
-
-  def new
   end
 
   def show
   end
 
+  def create
+    @item = Item.new(params_item)
+
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+      # need to log in for it to work
+  end
+
+  def new
+    @item = Item.new
+  end
+
+
   def destroy
+    @item.destroy
+    redirect_to items_path
   end
 
   private
 
-  def set_restaurant
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def params_item
+    params.require(:item).permit(
+      :location, :name, :description, :price, :condition, :deposit,
+      :image_url, :delivery, :policies
+    )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,36 @@
 
 Rental.destroy_all if Rails.env.development?
 Item.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
 
 
+peter_pan = User.create!(
+  first_name: "Peter",
+  last_name: "Peterson",
+  username: "peter_pan",
+  email: "peter_pan@fairy.com",
+  password: "user_peter_pan"
+  )
 
+cindy = User.create!(
+  first_name: "Cinderella",
+  last_name: "The Princess",
+  username: "Cindy",
+  email: "cindy@swamp.cz",
+  password: "user_cindy")
 
 
 speaker = Item.create!(
   location: "Montreal - Laval",
   name: "Great Powerfull Speaker",
-  description: "Set of 2 speakers Yamaha great for garden party super cool sound.",
+  description: "Set of 2 speakers Yamaha, great for garden party, super cool sound.",
   price: 10,
   condition: "1 year old. Great condition. Small hole on the top from cigarette butt.",
   deposit: 100,
   image_url: "https://www.listeningpost.co.nz/assets/Yamaaha-NS-5000-Speakers-Pair_183108_5.jpg",
   delivery: "pick up at my place",
-  policies: "if you break it or there is another hole from cigarette butt, you will buy me a new one"
+  policies: "if you break it or there is another hole from cigarette butt, you will buy me a new one",
+  user_id: peter_pan.id
 )
 
 dj_set = Item.create!(
@@ -34,8 +49,21 @@ dj_set = Item.create!(
   deposit: 500,
   image_url: "https://images-na.ssl-images-amazon.com/images/I/514J8t-BCSL._SL1032_.jpg",
   delivery: "pick up at Toronto, Maple Leaves stadium",
-  policies: "dont break it"
+  policies: "dont break it",
+  user_id: cindy.id
   )
 
+random_a = Rental.create!(
+  date: "2019-06-23",
+  status: "pending",
+  user_id: peter_pan.id,
+  item_id: dj_set.id
+  )
 
+random_b = Rental.create!(
+  date: "2019-05-04",
+  status: "approved",
+  user_id: cindy.id,
+  item_id: speaker.id
+  )
 
